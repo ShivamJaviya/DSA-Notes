a77303d57bcfd7e79c836875c0f2ff184e2795f3
Shivam Javiya <94696278+ShivamJaviya@users.noreply.github.com>
2024-02-11 15:02:31 +0530

    2Update intro.cpp

--- Array/intro.cpp
+++ Array/intro.cpp
@@ -14,5 +14,26 @@ cout << "\nThe numbers are: ";
 for (int i = 0; i < 5; ++i) {  
 cout << numbers[i] << "  ";  
 }  
-
+return 0;
 }  
+
+
+// C++ Program to Print Array Elements without Indexing 
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+
+//  Define an array 
+// 	int arr[] = { 11, 22, 33, 44 }; 
+
+// 	// Print elements of an array 
+// 	cout << "first element: " << *arr << endl; 
+// 	cout << "Second element: " << *(arr + 1) << endl; 
+// 	cout << "Third element: " << *(arr + 2) << endl; 
+// 	cout << "fourth element: " << *(arr + 3) << endl; 
+
+// 	return 0; 
+// }
+
